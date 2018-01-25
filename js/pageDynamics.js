387f5a8ee85141a1eb95d36a8dd483aa7e0848a2
danliszka <danliszka1@gmail.com>
2018-01-24 19:13:36 -0500

    made nav bar smoother when scrolling

--- js/pageDynamics.js
+++ js/pageDynamics.js
@@ -2,14 +2,10 @@ $(document).ready(function() {
   
   $(window).scroll(function () {
     if ($(window).scrollTop() > 62) {
-        $('nav').addClass('fixed-top');
-        $('nav').addClass('nav-scrolling');
-        $('.nav-buttons').addClass('nav-buttons-scrolling');
+        $('.nav-scrolling').removeClass('hide');
     }
     if ($(window).scrollTop() < 62) {
-        $('.nav-buttons').removeClass('nav-buttons-scrolling');
-        $('nav').removeClass('nav-scrolling');
-        $('nav').removeClass('fixed-top');
+        $('.nav-scrolling').addClass('hide');
     }
   });
 });
