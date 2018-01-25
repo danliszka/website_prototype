364f638ec3f109236eff482744a261685d6c3f6e
danliszka <danliszka1@gmail.com>
2018-01-24 20:22:46 -0500

    started bare bones for drop down menu in nav

--- js/pageDynamics.js
+++ js/pageDynamics.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
   
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 62) {
-        $('.nav-scrolling').removeClass('hide');
-    }
-    if ($(window).scrollTop() < 62) {
-        $('.nav-scrolling').addClass('hide');
-    }
-  });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 62) {
+            $('.nav-scrolling').removeClass('hide');
+        }
+        if ($(window).scrollTop() < 62) {
+            $('.nav-scrolling').addClass('hide');
+        }
+    });
+    
+    $('#about_us').click(function(ev){
+        console.log("hey");
+        ev.preventDefault();
+        ev.stopImmediatePropagation();
+        $('#drop_down').attr("style","display: inline-block");
+    });
+    
+    $(window).click(function(ev){
+        if ($('#drop_down').attr("style") == "display: inline-block")
+            {
+                $('#drop_down').attr("style","display: none");
+            }
+        console.log("not open");
+        
+    });
+    
+    
 });
